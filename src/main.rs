94ecd31e67ecf568bf29bd934b92ff700eb0a78f
wcagreen <corey.green12811@gmail.com>
2025-07-04 23:41:02 -0400

    Swapping to basic read_to_string for simplicity, adding iteration and splitting up record types into their own vec for further processing

--- src/main.rs
+++ src/main.rs
@@ -8,83 +8,58 @@ mod parser;
 
 use parser::{parse_carrier_record, parse_flight_record_legs, parse_segment_record};
 
-use polars::prelude::*;
-use pyo3::prelude::*;
-use pyo3::types::{PyDict, PyList};
-use records::{carrier_record, segment_records};
-use serde_json::json;
-use std::fs::File;
-use std::io::{self, BufRead, BufReader, BufWriter, Write};
-use std::iter::Peekable;
-// use std::path::Path;
-
-use records::flight_segment::FlightSegment;
-use  records::carrier_record::CarrierRecord;
+// use polars::prelude::*;
+// use pyo3::prelude::*;
+// use pyo3::types::{PyDict, PyList};
 
 
-use serde::Serialize;
+use std::fs::read_to_string;
 
-#[derive(Debug, Serialize)]
-pub struct SSIMRecordsJson {
-    carrier: Option<CarrierRecord>,
-    flights: Vec<FlightSegment>
+
+// fn parse_ssim_to_dataframe()
+
+fn read_all_ssim(file_path: &str) -> String {
+    let ssim_file: String = read_to_string(file_path).expect("Failed to read in file.");
+    return ssim_file;
+}
+
+fn ssim_iterator(ssim: String) {
+    let mut record_type_2 =  Vec::new();
+    let mut record_type_3s =  Vec::new();
+    let mut record_type_4s =  Vec::new();
+    for line in ssim.lines() {
+        let record_type = line.chars().nth(0);
+
+        if record_type == Some('2') {
+
+            let parsed_record_type_2 =  parse_carrier_record(line);
+            // println!("{:?}", parsed_record_type_2);
+            record_type_2.push(parsed_record_type_2);
+
+        } else if record_type == Some('3') {
+
+            let parsed_record_type_3 =  parse_flight_record_legs(line);
+            // println!("{:?}", parsed_record_type_3);
+            record_type_3s.push(parsed_record_type_3);
+
+        } else if record_type == Some('4') {
+
+            let parsed_record_type_4 =  parse_segment_record(line);
+            record_type_4s.push(parsed_record_type_4);
+            // println!("{:?}", parsed_record_type_4);
+        } 
+
+    }
+    println!("{:?}", record_type_3s);
 }
 
 
-
 fn main() {
-    let read_buffer_size = None;
+
     let file_path = "test_files/multi_ssim.dat";
-    let file = File::open(file_path).expect("Failed to open file");
-    let reader = match read_buffer_size {
-        Some(size) => BufReader::with_capacity(size, file),
-        None => BufReader::new(file),
-    };
+    let ssim = read_all_ssim(file_path);
+    ssim_iterator(ssim);
 
-    let mut lines: Peekable<_> = reader.lines().peekable();
-    let mut records = Vec::new();
-    let mut carrier = None;
-
-    while let Some(Ok(line)) = lines.next() {
-
-        if line.starts_with('2') {
-            carrier = parse_carrier_record(&line);
-        }
-        
-        if line.starts_with('3') {
-            let flight_record = parse_flight_record_legs(&line);
-            let mut segment_records = Vec::new();
-
-            while let Some(Ok(peek_line)) = lines.peek() {
-                if peek_line.starts_with('4') {
-                    segment_records.push(parse_segment_record(peek_line));
-                    lines.next(); 
-                } else {
-                    break; 
-                }
-            }
-
-            if let Some(flight_leg) = flight_record {
-                records.push(FlightSegment {
-                    flight_leg,
-                    segments: segment_records,
-                });
-            }
-        
-        }
-    }
-
-
-    let ssim_json = SSIMRecordsJson {
-        carrier,
-        flights: records, 
-    };
-    let json = serde_json::to_string_pretty(&ssim_json).unwrap();
-
-    let output_file_path = "";
-    let output_file = File::create(output_file_path).expect("Failed to create file");
-    serde_json::to_writer_pretty(output_file, &ssim_json)
-        .expect("Failed to write JSON to file");
 
 }
 
