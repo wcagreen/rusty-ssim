d5ae7e744fe9af65b766a274e4f87b8979106a3f
wcagreen <corey.green12811@gmail.com>
2025-07-25 23:09:40 -0400

    Cleaning up main, removing unneeded files and updating gitignore.

--- flight_records.rs
+++
@@ -1,9 +0,0 @@
-use serde::Serialize;
-use crate::records::flight_leg_records::FlightLegRecord;
-use crate::records::segment_records::SegmentRecords;
-
-#[derive(Debug, Serialize)]
-pub struct FlightSegment<'a>  {
-    pub flight_leg: FlightLegRecord<'a>,
-    pub segments: Vec<SegmentRecords<'a>>,
-}
