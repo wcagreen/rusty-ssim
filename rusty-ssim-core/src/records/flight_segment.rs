e34f2045935dc07003647cf381377383b38aeb63
wcagreen <corey.green12811@gmail.com>
2025-08-12 22:55:32 -0400

    cleaning: Removing dead code.

--- rusty-ssim-core/src/records/flight_segment.rs
+++
@@ -1,9 +0,0 @@
-use crate::records::flight_leg_records::FlightLegRecord;
-use crate::records::segment_records::SegmentRecords;
-use serde::Serialize;
-
-#[derive(Debug, Serialize)]
-pub struct FlightSegment {
-    pub flight_leg: FlightLegRecord,
-    pub segments: Vec<Option<SegmentRecords>>,
-}
