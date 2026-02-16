abcc6bba64566bdb2721005663b3fc93d14dc53f
wcagreen <corey.green12811@gmail.com>
2026-02-16 00:34:38 -0500

    Resolving docs failures

--- rs-rusty-ssim/src/lib.rs
+++ rs-rusty-ssim/src/lib.rs
@@ -43,6 +43,7 @@ pub use rusty_ssim_core::{ssim_to_csv, ssim_to_dataframe, ssim_to_dataframes, ss
 /// Re-exported [Polars](https://pola.rs) crate for working with the DataFrames
 /// returned by this library.
 ///
-/// Includes ["lazy", "parquet", "dtype-struct", "ipc", "performant", "json"] needed by rustyssim.
+/// Includes `["lazy", "parquet", "dtype-struct", "ipc", "json"]` needed by rustyssim.
+/// The `performant` feature is also enabled by default.
 /// For additional features, add `polars` directly to your `Cargo.toml` —
 pub use polars;
